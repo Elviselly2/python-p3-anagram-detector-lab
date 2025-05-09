3943ae21baa31266825e41671a8be2edfec22f6f
nosmall <elvisotieno24@gmail.com>
2025-05-09 16:40:15 +0300

    adds content

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        anagrams = [word for word in word_list if sorted(word.lower()) == sorted_word and word.lower() != self.word]
+
+        return anagrams
